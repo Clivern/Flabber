f23ea4cf999b936b9f79e8426cdb66513d5cb8cb
Clivern <hello@clivern.com>
2021-08-27 21:26:40 +0200

    update

---
+++ guide/basic-types.js
@@ -0,0 +1,29 @@
+var x = 'H';
+var y = true;
+var z = 2;
+// Array
+var k = ['A', 'B', 'C'];
+// Function
+var sum;
+sum = function (x, y) {
+    return x + y;
+};
+// Object
+var person;
+person = {
+    name: "Joe",
+    age: 46
+};
+var info = { address: '1234 Anywhere St.', city: 'Seattle' };
+var j = {
+    name: "Doe",
+    age: 47
+};
+// Typed arrays
+var p = [
+    { name: 'a', age: 8 }
+];
+p.push({
+    name: 'b',
+    age: 26
+});
